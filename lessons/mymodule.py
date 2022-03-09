577a1d73c391453957a3a8665bfcf2d561d5e4a4
Michael <michael@ou.edu>
2022-03-09 11:14:33 -0600

    Worked on lesson 5

--- lessons/mymodule.py
+++ lessons/mymodule.py
@@ -1,7 +1,10 @@
-# Import numpy once at the start!
 import numpy as np
 
-# Define the function here
+def only_evens_numpy(input_array):
+    '''Takes in an array and returns an array with only the even values'''
+    bool_array = input_array % 2 == 0
+    return input_array[bool_array]
+
 def only_evens(list_of_numbers):
     """Take a list of numbers, and return a list of only the even numbers"""
 
@@ -19,18 +22,3 @@ def only_evens(list_of_numbers):
 
     # Then return the number
     return even_numbers
-
-def only_evens_numpy(list_of_numbers):
-    """Take a numpy array of numbers and return a numpy array of the even values."""
-    
-    # Make sure the list of numbers is an array
-    array_of_numbers = np.array(list_of_numbers)
-    
-    # Find the even numbers in the array
-    evens_mask = array_of_numbers % 2 == 0
-    
-    # Apply mask to list of numbers to get only evens
-    even_numbers_array = array_of_numbers[evens_mask]
-    
-    # Then return array of even numbers
-    return even_numbers_array
